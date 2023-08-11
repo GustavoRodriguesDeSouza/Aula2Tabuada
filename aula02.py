b09e134ae1f07e83ab97d5c4ec76c388750fdfc7
Gustavo <129074506+GustavoRodriguesDeSouza@users.noreply.github.com>
2023-08-11 11:19:26 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,17 @@
+# Tabuada
+N1 = int(input('Digite o número para ver a tabuada dele: '))
+print('{} x {} = {:2}'.format(N1, 1, N1*1))
+print('{} x {} = {:2}'.format(N1, 2, N1*2))
+print('{} x {} = {:2}'.format(N1, 3, N1*3))
+print('{} x {} = {:2}'.format(N1, 4, N1*4))
+print('{} x {} = {:2}'.format(N1, 5, N1*5))
+print('{} x {} = {:2}'.format(N1, 6, N1*6))
+print('{} x {} = {:2}'.format(N1, 7, N1*7))
+print('{} x {} = {:2}'.format(N1, 8, N1*8))
+print('{} x {} = {:2}'.format(N1, 9, N1*9))
+print('{} x {} = {:2}'.format(N1, 10, N1*10))
+
+
+
+
+
---
+++ aula2.py
@@ -0,0 +1,12 @@
+# Tabuada
+N01 = int(input('Digite o número para ver a tabuada dele: '))
+print('{} / {} = {:1}'.format(N01, 1, N01/1))
+print('{} / {} = {:2}'.format(N01, 2, N01/2))
+print('{} / {} = {:2}'.format(N01, 3, N01/3))
+print('{} / {} = {:2}'.format(N01, 4, N01/4))
+print('{} / {} = {:2}'.format(N01, 5, N01/5))
+print('{} / {} = {:2}'.format(N01, 6, N01/6))
+print('{} / {} = {:2}'.format(N01, 7, N01/7))
+print('{} / {} = {:2}'.format(N01, 8, N01/8))
+print('{} / {} = {:2}'.format(N01, 9, N01/9))
+print('{} / {} = {:2}'.format(N01, 10, N01/10))
